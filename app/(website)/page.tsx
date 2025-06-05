9d4a54aa2a40e29aa3431dc673d699514fb79d7a
amitchackroborty04 <amitck803@gmail.com>
2025-06-05 16:22:09 +0600

    done future frmas

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,3 +1,4 @@
+import Featured_Farms from '@/components/Featured_Farms'
 import HeroSection from '@/components/Hero'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
     <div>
       <HeroSection/>
+      <Featured_Farms/>
     </div>
   )
 }
